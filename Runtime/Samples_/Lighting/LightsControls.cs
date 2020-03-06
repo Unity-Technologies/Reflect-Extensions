946e4b8589718a55a5192c22b409019b14507f11
Fred Moreau <50830458+FredMoreau@users.noreply.github.com>
2020-03-06 01:55:23 +0100

    Misc Helpers

---
+++ Runtime/Samples_/Lighting/LightsControls.cs
@@ -0,0 +1,23 @@
+﻿namespace UnityEngine.Reflect.Extensions.Lighting
+{
+    /// <summary>
+    /// LightsControls
+    /// Turns all lights in hierarchy on/off on enable/disable
+    /// </summary>
+    [AddComponentMenu("Reflect/Lighting/Lights Controls")]
+    [ExecuteAlways]
+    public class LightsControls : MonoBehaviour
+    {
+        private void OnEnable()
+        {
+            foreach (Light light in GetComponentsInChildren<Light>())
+                light.enabled = true;
+        }
+
+        private void OnDisable()
+        {
+            foreach (Light light in GetComponentsInChildren<Light>())
+                light.enabled = false;
+        }
+    }
+}
---
+++ Runtime/Samples_/Timeline/AmbientLightControlTrack.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
+
+namespace ReflectWorkshop
+{
+    [TrackClipType(typeof(AmbientLightControlAsset))]
+    [TrackBindingType(null)]
+    public class AmbientLightControlTrack : TrackAsset
+    {
+        public override Playable CreateTrackMixer(PlayableGraph graph, GameObject go, int inputCount)
+        {
+            return ScriptPlayable<AmbientLightControlMixerBehaviour>.Create(graph, inputCount);
+        }
+    }
+}
