ff3eca35c8573ef00a16ef58effbe1a15f41d5ca
brian_bare <brian.bare@unity3d.com>
2020-04-01 16:03:19 -0400

    Simple Top Menu Toggle
    (cherry picked from commit e84762cc4fbc2730f7dc57393b38bde4c013d3ec)

---
+++ Samples/ViewerTopMenus/SimpleTopMenuToggle.cs
@@ -0,0 +1,42 @@
+﻿namespace UnityEngine.Reflect.Extensions
+{
+    /// <summary>
+    /// Simple Top Menu that turns its UI on and off
+    /// </summary>
+    public class SimpleTopMenuToggle : TopMenu
+    {
+        bool activated;
+
+        void OnEnable()
+        {
+            OnVisiblityChanged += CheckVisibility;
+        }
+
+        void OnDisable()
+        {
+            OnVisiblityChanged -= CheckVisibility;
+        }
+
+        // Use Top Menu's visibilty event to know if the button is activated or not
+        void CheckVisibility(bool visible)
+        {
+            activated = visible;
+        }
+
+        /// <summary>
+        /// If button is clicked hide or show appropriately and call disabling/enabling methods on the different menus
+        /// </summary>
+        public override void OnClick()
+        {
+            if (activated)
+            {
+                Deactivate();
+                ShowButtons();
+            }
+            else
+            {
+                base.OnClick();
+            }
+        }
+    }
+}
