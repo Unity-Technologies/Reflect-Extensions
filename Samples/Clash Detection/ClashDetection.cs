8c886ebb71b0bba21502f29b0f3e84e4bcc84796
brian_bare <brian.bare@unity3d.com>
2020-04-01 17:29:27 -0400

    Working on updating and modifying scripts

--- Samples/Clash Detection/ClashDetection.cs
+++ Samples/Clash Detection/ClashDetection.cs
@@ -1,92 +1,149 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.Reflect;
+﻿using System.Collections.Generic;
 using UnityEngine.UI;
 
-public class ClashDetection : MonoBehaviour
+namespace UnityEngine.Reflect.Extensions
 {
-    public InputField inputField1;
-    public InputField inputField2;
-    public Material highlightMaterial;
-
-    public string clashCategory1;
-    public string clashCategory2;
-    public GameObject[] FilteredObjects1;
-    public GameObject[] FilteredObjects2;
-    public List<GameObject> ClashingObjects;
-
-    List<GameObject> Highlights;
-
-    // Start is called before the first frame update
-    void Start()
+    public class ClashDetection : MonoBehaviour, IObserveReflectRoot
     {
-        Highlights = new List<GameObject>();
-        StartCoroutine(RepeatCheck());
-    }
+        public InputField inputField1;
+        public InputField inputField2;
+        public Material highlightMaterial;
 
-    public void SetName()
-    {
-        clashCategory1 = inputField1.text;
-        clashCategory2 = inputField2.text;
-        Debug.Log(clashCategory1);
-        Debug.Log(clashCategory2);
+        public string clashCategory1;
+        public string clashCategory2;
+        public List<GameObject> filteredObjects1;
+        public List<GameObject> filteredObjects2;
+        public List<GameObject> ClashingObjects;
 
-        StartCoroutine(RepeatCheck());
-    }
+        List<GameObject> Highlights;
 
-    [ContextMenu("Check for Clashes")]
-    public void CheckClashes()
-    {
-        ClashingObjects.Clear();
-        Metadata[] metadatas = FindObjectsOfType<Metadata>();
-        FilteredObjects1 = MetadataUtilities.FilterbyCategory(metadatas, clashCategory1);
-        FilteredObjects2 = MetadataUtilities.FilterbyCategory(metadatas, clashCategory2);
-
-        foreach (var filteredObjects1 in FilteredObjects1)
+        void OnEnable()
         {
-            foreach (var filteredObjects2 in FilteredObjects2)
+            ReflectMetadataManager.Instance.Attach(this, new MetadataSearch("Category", ReflectMetadataManager.Instance.AnyValue, false));
+        }
+
+        void OnDisable()
+        {
+            ReflectMetadataManager.Instance.Detach(this);
+        }
+
+        void Start()
+        {
+            Highlights = new List<GameObject>();
+        }
+
+        public void SetName()
+        {
+            clashCategory1 = inputField1.text;
+            clashCategory2 = inputField2.text;
+            CheckClashes();
+        }
+
+        [ContextMenu("Check for Clashes")]
+        void RunInEditor()
+        {
+            filteredObjects1 = new List<GameObject>();
+            filteredObjects2 = new List<GameObject>();
+            ClashingObjects.Clear();
+            Metadata[] metas = FindObjectsOfType<Metadata>();
+            foreach (var meta in metas)
             {
-                if (filteredObjects1.GetComponent<Renderer>().bounds.Intersects(
-                    filteredObjects2.GetComponent<Renderer>().bounds))
+                if (meta.GetParameter("Category") == clashCategory1)
                 {
-                    ClashingObjects.Add(filteredObjects1);
-                    ClashingObjects.Add(filteredObjects2);
+                    if (!filteredObjects1.Contains(meta.gameObject))
+                        filteredObjects1.Add(meta.gameObject);
                 }
+                else if (meta.GetParameter("Category") == clashCategory2)
+                {
+                    if (!filteredObjects2.Contains(meta.gameObject))
+                        filteredObjects2.Add(meta.gameObject);
+                }
+            }
+
+            CheckClashes();
+        }
+
+        public void CheckClashes()
+        {
+            //ClashingObjects.Clear();
+            //Metadata[] metadatas = FindObjectsOfType<Metadata>();
+            //filteredObjects1 = MetadataUtilities.FilterbyCategory(metadatas, clashCategory1);
+            //filteredObjects2 = MetadataUtilities.FilterbyCategory(metadatas, clashCategory2);
+
+            foreach (var filteredObjects1 in filteredObjects1)
+            {
+                foreach (var filteredObjects2 in filteredObjects2)
+                {
+                    if (filteredObjects1.GetComponent<Renderer>().bounds.Intersects(
+                        filteredObjects2.GetComponent<Renderer>().bounds))
+                    {
+                        ClashingObjects.Add(filteredObjects1);
+                        ClashingObjects.Add(filteredObjects2);
+                    }
+                }
+            }
+
+            HighlightClashes();
+        }
+
+
+        void HighlightClashes()
+        {
+            foreach (var item in Highlights)
+            {
+                GameObject.DestroyImmediate(item);
+            }
+            
+            Highlights.Clear();
+            Highlights = new List<GameObject>();
+
+            foreach (var item in ClashingObjects)
+            {
+                Bounds itemBounds = item.GetComponent<Renderer>().bounds;
+                GameObject highlight = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                highlight.transform.SetParent(item.transform);
+                highlight.transform.position = itemBounds.center;
+                highlight.transform.localScale = new Vector3(
+                    itemBounds.size.x * 1.01f, itemBounds.size.y * 1.01f, itemBounds.size.z * 1.01f);
+                highlight.GetComponent<MeshRenderer>().material = highlightMaterial;
+
+                Highlights.Add(highlight);
             }
         }
 
-        HighlightClashes();
-    }
-
-
-    private void HighlightClashes()
-    {
-        foreach (var item in Highlights)
+        #region IObserveReflectRoot implementation
+        /// <summary>
+        /// What to do before searching Metadata components
+        /// </summary>
+        public void NotifyBeforeSearch()
         {
-            GameObject.DestroyImmediate(item);
-        }        {
-        Highlights.Clear();
+            filteredObjects1 = new List<GameObject>();
+            filteredObjects2 = new List<GameObject>();
+            ClashingObjects.Clear();
         }
-        Highlights = new List<GameObject>();
 
-        foreach (var item in ClashingObjects)
+        /// <summary>
+        /// What to do when a Metadata parameter is found
+        /// </summary>
+        /// <param name="reflectObject">The GameObject with the matching Metadata search pattern</param>
+        /// <param name="result">The value of the found parameter in the Metadata component</param>
+        public void NotifyReflectRootObservers(GameObject reflectObject, string result = null)
         {
-            Bounds itemBounds = item.GetComponent<Renderer>().bounds;
-            GameObject highlight = GameObject.CreatePrimitive(PrimitiveType.Cube);
-            highlight.transform.SetParent(item.transform);
-            highlight.transform.position = itemBounds.center;
-            highlight.transform.localScale = new Vector3(
-                itemBounds.size.x*1.01f, itemBounds.size.y*1.01f, itemBounds.size.z*1.01f);
-            highlight.GetComponent<MeshRenderer>().material = highlightMaterial;
-
-            Highlights.Add(highlight);
+            if (reflectObject != null && !string.IsNullOrEmpty(result))
+            {
+                Debug.Log(reflectObject);
+                if (clashCategory1 == result && !filteredObjects1.Contains(reflectObject))
+                    filteredObjects1.Add(reflectObject);
+                else if (clashCategory2 == result && !filteredObjects2.Contains(reflectObject))
+                    filteredObjects2.Add(reflectObject);
+            }
         }
-    }
 
-    IEnumerator RepeatCheck()
-    {
-        yield return new WaitForSeconds(2f);
-        CheckClashes(); 
+        /// <summary>
+        /// What to do after finishing the search on Metatdata components
+        /// </summary>
+        public void NotifyAfterSearch()
+        { }
+        #endregion
     }
 }
---
+++ Samples/Material Swapping/CategoryMaterialSwapping.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections.Generic;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace UnityEngine.Reflect.Extensions
+{
+    public class CategoryMaterialSwapping : MonoBehaviour, IObserveReflectRoot
+    {
+        public string categoryToPaint;
+        public Material newMaterial;
+        List<GameObject> filteredObjects;
+
+        void OnEnable()
+        {
+            ReflectMetadataManager.Instance.Attach(this, new MetadataSearch("Category", categoryToPaint, false));
+        }
+
+        void OnDisable()
+        {
+            ReflectMetadataManager.Instance.Detach(this);
+        }
+
+        [ContextMenu("Override Materials")]
+        void RunInEditor()
+        {
+            filteredObjects = new List<GameObject>();
+            Metadata[] metas = FindObjectsOfType<Metadata>();
+
+            foreach (var meta in metas)
+            {
+                if (meta.GetParameter("Category") == categoryToPaint)
+                {
+                    if (!filteredObjects.Contains(meta.gameObject))
+                        filteredObjects.Add(meta.gameObject);
+                }
+            }
+            SwapMaterialByCategory();
+        }
+
+        public void SwapMaterialByCategory()
+        {
+            foreach (var filteredObject in filteredObjects)
+            {
+                MeshRenderer meshRend = filteredObject.GetComponent<MeshRenderer>();
+
+                if (Application.isPlaying)
+                {
+                    Material[] newMatArray = new Material[meshRend.materials.Length];
+                    for (int i = 0; i < newMatArray.Length; i++)
+                    {
+                        newMatArray[i] = newMaterial;
+                    }
+
+                    meshRend.materials = newMatArray;
+                }
+                else
+                {
+                    Material[] newMatArray = new Material[meshRend.sharedMaterials.Length];
+                    for (int i = 0; i < newMatArray.Length; i++)
+                    {
+                        newMatArray[i] = newMaterial;
+                    }
+#if UNITY_EDITOR
+                    Undo.RecordObject(meshRend, "Override Materials");
+                    PrefabUtility.RecordPrefabInstancePropertyModifications(meshRend);
+#endif
+                    meshRend.sharedMaterials = newMatArray;
+                }
+
+
+            }
+        }
+
+        #region IObserveReflectRoot implementation
+        /// <summary>
+        /// What to do before searching Metadata components
+        /// </summary>
+        public void NotifyBeforeSearch()
+        {
+            filteredObjects = new List<GameObject>();
+        }
+
+        /// <summary>
+        /// What to do when a Metadata parameter is found
+        /// </summary>
+        /// <param name="reflectObject">The GameObject with the matching Metadata search pattern</param>
+        /// <param name="result">The value of the found parameter in the Metadata component</param>
+        public void NotifyReflectRootObservers(GameObject reflectObject, string result = null)
+        {
+            if (reflectObject != null)
+            {
+                if (!filteredObjects.Contains(reflectObject))
+                    filteredObjects.Add(reflectObject);
+            }
+        }
+
+
+        /// <summary>
+        /// What to do after finishing the search on Metatdata components
+        /// </summary>
+        public void NotifyAfterSearch()
+        { }
+        #endregion
+    }
+}
